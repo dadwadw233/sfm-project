e18a781fe77f14bbd28ad7feaea2049affd986ed
forgetable233 <1456882404@qq.com>
2022-01-29 22:03:59 +0800

    push

--- pose_estimate.cc
+++ pose_estimate.cc
@@ -7,21 +7,21 @@ namespace sfmProject {
 template <typename T>
 bool pose_estimate::PnPCeres::operator()(const T *const camera,
                                          T *residual) const {
-//  T p[3];
-//  T point[3];
-//  point[0] = T(_xyz.x);
-//  point[1] = T(_xyz.y);
-//  point[2] = T(_xyz.z);
-//  ceres::AngleAxisRotatePoint(camera, point, p);  // compute RP //
-//  p[0] += camera[3];
-//  p[1] += camera[4];
-//  p[2] += camera[5];
-//  T xp = p[0] / p[2];
-//  T yp = p[1] / p[2];  // xp,yp is Normalize coordinates，the depth is p[2] //
-//  T u_ = xp * K.at<double>(0, 0) + K.at<double>(0, 2);
-//  T v_ = yp * K.at<double>(1, 1) + K.at<double>(1, 2);
-//  residual[0] = T(_uv.x) - u_;
-//  residual[1] = T(_uv.y) - v_;
+  T p[3];
+  T point[3];
+  point[0] = T(_xyz.x);
+  point[1] = T(_xyz.y);
+  point[2] = T(_xyz.z);
+  ceres::AngleAxisRotatePoint(camera, point, p);  // compute RP //
+  p[0] += camera[3];
+  p[1] += camera[4];
+  p[2] += camera[5];
+  T xp = p[0] / p[2];
+  T yp = p[1] / p[2];  // xp,yp is Normalize coordinates，the depth is p[2] //
+  T u_ = xp * K.at<double>(0, 0) + K.at<double>(0, 2);
+  T v_ = yp * K.at<double>(1, 1) + K.at<double>(1, 2);
+  residual[0] = T(_uv.x) - u_;
+  residual[1] = T(_uv.y) - v_;
   return true;
 }
 ceres::CostFunction *pose_estimate::PnPCeres::Create(const cv::Point2f &uv,
